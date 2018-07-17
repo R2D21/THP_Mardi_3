21f4491212a5566d77b38223f4acec0228ec1c92
remi <remi.boivin@epitech.eu>
2018-07-17 17:58:47 +0200

    modif board.rb

--- board.rb
+++ board.rb
@@ -3,9 +3,9 @@
 class Board
 
   def initialize
-    #TO DO :
-    #Quand la classe s'initialize, elle doit créer 9 instances BoardCases
-    #Ces instances sont rangées dans une array qui est l'attr_accessor de la classe
+
+#we create instances for all of the 9 cases
+
     left_up = BoardCase.new
     up_center = BoardCase.new
     up_right = BoardCase.new
@@ -16,24 +16,40 @@ class Board
     down_middle = BoardCase.new
     down_right = BoardCase.new
 
+#we put all the cases inside of an array in order later to know the position of each of the cases
     @board = [left_up, up_center, up_right,
               middle_left, middle_center, middle_right,
               down_left,down_middle, down_right]
   end
   def print_board
-    #TO DO : doit renvoyer la valeur au format string
+    #Method that prints the board game
+    #We display the values of each of the cases according to their index, taking the value 'boardcase.new'
+
     puts "          " + @board[0].status + " |" + @board[1].status + " |" + @board[2].status
     puts "        ---------"
     puts "          " + @board[3].status + " |" + @board[4].status + " |" + @board[5].status
     puts "        ---------"
     puts "          " + @board[6].status + " |" + @board[7].status + " |" + @board[8].status
   end
-  def is_win()
-    if @board[0] == @board[1] && @board[0] == @board[2]
+
+  #We create a method that checks out if the player wins
+  def win
+    if @array[0] == @array[1] && @array[0] == @array[2]
       return true
-    elsif @board[3] == @board[4] && @board[3] == @board[5]
+    elsif  @array[3] == @array[4] &&  @array[3] == @array[5]
       return true
-    elsif @board[6] == @board[7] && @board[6] == @board[8]
+    elsif  @array[6] == @array[7] &&  @array[6] == @array[8]
       return true
+    elsif  @array[0] == @array[3] &&  @array[0] == @array[6]
+      return true
+    elsif  @array[1] == @array[4] &&  @array[1] == @array[7]
+      return true
+    elsif  @array[2] == @array[5] &&  @array[2] == @array[8]
+      return true
+    elsif  @array[0] == @array[4] &&  @array[0] == @array[8]
+      return true
+    elsif  @array[2] == @array[4] &&  @array[2] == @array[6]
+      return true
+    end
   end
 end
