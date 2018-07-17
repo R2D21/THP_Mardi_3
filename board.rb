eec0299b4b5fb5a8709b7e0fa442fd491ee72332
jjeleven11 <40149119+jjeleven11@users.noreply.github.com>
2018-07-17 16:09:38 +0200

    Update board.rb

--- board.rb
+++ board.rb
@@ -3,7 +3,8 @@
 class Board
 
   def initialize
-        #we create instances for all of the 9 cases
+    
+#we create instances for all of the 9 cases
 
     left_up = BoardCase.new
     up_center = BoardCase.new
@@ -14,14 +15,15 @@ class Board
     down_left = BoardCase.new
     down_middle = BoardCase.new
     down_right = BoardCase.new
+    
 #we put all the cases inside of an array in order to know the position of each of the cases
-
     @board = [left_up, up_center, up_right,
               middle_left, middle_center, middle_right,
               down_left,down_middle, down_right]
   end
   def print_board
- #We display the values of each of the cases according to their index, taking the value 'boardcase.new'
+    
+#We display the values of each of the cases according to their index, taking the value 'boardcase.new'
     puts "          " + @board[0].status + " |" + @board[1].status + " |" + @board[2].status
     puts "        ---------"
     puts "          " + @board[3].status + " |" + @board[4].status + " |" + @board[5].status
