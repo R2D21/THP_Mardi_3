e5b94b709cec2bfd6421b8811fe4dcb09d5c5a29
jjeleven11 <jjdraa@gmail.com>
2018-07-17 18:54:36 +0200

    modif board rb

--- board.rb
+++ board.rb
@@ -4,7 +4,7 @@ class Board
 
   def initialize
 
-#we create instances for all of the 9 cases inside the board
+#we create instances for all of the 9 cases of the board
 
     left_up = BoardCase.new
     up_center = BoardCase.new
@@ -16,40 +16,39 @@ class Board
     down_middle = BoardCase.new
     down_right = BoardCase.new
 
-#we put all the cases inside of an array in order to later know the position of each of the cases
+#we put all the cases inside of an array in order later to know the position of each of the cases
     @board = [left_up, up_center, up_right,
               middle_left, middle_center, middle_right,
               down_left,down_middle, down_right]
   end
-
-#Method that prints the board game
   def print_board
-#We display the values of each of the cases according to their index, taking the value 'boardcase.new'
-#the .status variable then defines if each of the cases are BLANK or either outputs an X or an O
+    #Method that prints the board game
+    #We display the values of each of the cases according to their index, taking the value 'boardcase.new'
 
     puts "          " + @board[0].status + " |" + @board[1].status + " |" + @board[2].status
+    puts "        ---------"
     puts "          " + @board[3].status + " |" + @board[4].status + " |" + @board[5].status
     puts "        ---------"
     puts "          " + @board[6].status + " |" + @board[7].status + " |" + @board[8].status
   end
 
-#We create a method that checks out if the player wins according the differents alignements
+  #We create a method that checks out if the player wins
   def win
-    if @array[0] == @array[1] && @array[0] == @array[2] #top horizontal line
+    if @array[0] == @array[1] && @array[0] == @array[2]
       return true
-    elsif  @array[3] == @array[4] &&  @array[3] == @array[5] #middle horizontal line
+    elsif  @array[3] == @array[4] &&  @array[3] == @array[5]
       return true
-    elsif  @array[6] == @array[7] &&  @array[6] == @array[8] #bottom horizontal line
+    elsif  @array[6] == @array[7] &&  @array[6] == @array[8]
       return true
-    elsif  @array[0] == @array[3] &&  @array[0] == @array[6] #top vertical line
+    elsif  @array[0] == @array[3] &&  @array[0] == @array[6]
       return true
-    elsif  @array[1] == @array[4] &&  @array[1] == @array[7] #middle vertical line
+    elsif  @array[1] == @array[4] &&  @array[1] == @array[7]
       return true
-    elsif  @array[2] == @array[5] &&  @array[2] == @array[8] #bottom vertical line
+    elsif  @array[2] == @array[5] &&  @array[2] == @array[8]
       return true
-    elsif  @array[0] == @array[4] &&  @array[0] == @array[8] #diagonal line
+    elsif  @array[0] == @array[4] &&  @array[0] == @array[8]
       return true
-    elsif  @array[2] == @array[4] &&  @array[2] == @array[6] #diagonal line
+    elsif  @array[2] == @array[4] &&  @array[2] == @array[6]
       return true
     end
   end
